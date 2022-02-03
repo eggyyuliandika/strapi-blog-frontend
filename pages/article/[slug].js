5052e2fe4350614d1d5b1242d165cf5c26d69eaf
sarascahya <sarascahya@gmail.com>
2022-02-03 20:43:52 +0800

    :sparkles: checking bug

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -2,29 +2,15 @@ import ReactMarkdown from "react-markdown";
 import Moment from "react-moment";
 import { fetchAPI } from "../../lib/api";
 import Layout from "../../components/layout";
-import Image from "../../components/image";
-// import Seo from "../../components/seo";
-
 import { getStrapiMedia } from "../../lib/media";
 
 const Article = (props) => {
-  let { article } = props
-  article = article.attributes
+  let { article } = props;
 
-  // console.log(article)
- 
-  const imageUrl = getStrapiMedia(article.image.data.attributes);
-   
-  // const seo = {
-  //   metaTitle: article.title,
-  //   metaDescription: article.description,
-  //   shareImage: article.image,
-  //   article: true,
-  // };
+  const imageUrl = getStrapiMedia(article?.attributes?.image?.data?.attributes);
 
   return (
-    <Layout categories={categories}>
-      {/* <Seo seo={seo} /> */}
+    <Layout>
       <div
         id="banner"
         className="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-padding uk-margin"
@@ -32,31 +18,24 @@ const Article = (props) => {
         data-srcset={imageUrl}
         data-uk-img
       >
-        <h1>{article?.title} </h1>
+        <h1>{article?.attributes?.title} </h1>
       </div>
       <div className="uk-section">
         <div className="uk-container uk-container-small">
-          <ReactMarkdown children={article?.content} escapeHtml={false} />
+          <ReactMarkdown
+            children={article?.attributes?.content}
+            escapeHtml={false}
+          />
           <hr className="uk-divider-small" />
           <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
-            <div>
-              {/* {article?.author?.data?.attributes?.picture && (
-                <Image
-                  image={article?.attributes?.picture}
-                  style={{
-                    position: "static",
-                    borderRadius: "50%",
-                    height: 30,
-                  }}
-                />
-              )} */}
-            </div>
             <div className="uk-width-expand">
               <p className="uk-margin-remove-bottom">
                 By {article?.attributes?.author?.data?.attributes?.name}
               </p>
               <p className="uk-text-meta uk-margin-remove-top">
-                <Moment format="MMM Do YYYY">{article?.attributes?.published_at}</Moment>
+                <Moment format="MMM Do YYYY">
+                  {article?.attributes?.published_at}
+                </Moment>
               </p>
             </div>
           </div>
@@ -79,11 +58,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const articles = await fetchAPI(`/articles?filters[slug][$eq]=${params.slug}&populate=*`);
-  const categories = await fetchAPI("/categories");
+  const articles = await fetchAPI(
+    `/articles?filters[slug][$eq]=${params.slug}&populate=*`
+  );
 
   return {
-    props: { article: articles.data[0], categories },
+    props: { article: articles.data[0] },
     revalidate: 1,
   };
 }
